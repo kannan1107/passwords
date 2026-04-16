13033c61e05e67679cf49eaa06f359722cbb00c9
kannan1107 <kannan11071985@gmail.com>
2026-04-16 19:40:09 +0530

    Add build configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
 
-export default defineConfig({
-  build: {
+   build: {
     outDir: 'dist'
   }
-})
+});
+
+
