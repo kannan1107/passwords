c8c23ee36fdd1b1f14fc3b3f4a99320600a958da
kannan1107 <kannan11071985@gmail.com>
2026-04-29 17:31:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from './component/Navbar.jsx'
 import Home from './component/Home.jsx'
 import Login from './component/Login.jsx'
 import Signup from './component/Signup.jsx'
+import ForgetPassword from './component/forgetPassword.jsx'
+import ResetPassword from './component/resetPassword.jsx'
 import RestartPassword from './component/restartPassword.jsx'
 import UpdatePassword from './component/updatePassword.jsx'
 
@@ -24,9 +26,12 @@ function App() {
         {/* <Route path="/contact" element={<Contact/>} /> */}
        
         <Route path="/signup" element={<Signup/>} />
+        <Route path="/forgot-password" element={<ForgetPassword/>} />
         <Route path="/restartpassword" element={<RestartPassword/>} />
+        <Route path="/resetPassword" element={<ResetPassword/>} />
+        <Route path="/resetPassword/:id/:token" element={<ResetPassword/>} />
         <Route path="*" element={"404 Not Found"} />
-        <Route path='updatePassword' element={<UpdatePassword/>} />
+        <Route path="/updatePassword" element={<UpdatePassword/>} />
       </Routes>
     </BrowserRouter>
      
